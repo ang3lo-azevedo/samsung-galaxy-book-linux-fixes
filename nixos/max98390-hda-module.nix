528d0befc9744042adc980ebf22c6087b12e7d18
Angelo Azevedo <ang3lo@azevedos.eu.org>
2026-04-04 19:08:55 +0100

    Refactor max98390-hda module to support Clang and LLVM toolchain

--- nixos/max98390-hda-module.nix
+++ nixos/max98390-hda-module.nix
@@ -1,23 +1,35 @@
-{ stdenv, lib, kernel, fetchFromGitHub }:
+{ stdenvNoCC, lib, kernel, fetchFromGitHub, llvmPackages, gcc }:
 
-stdenv.mkDerivation {
+let
+  kernelUsesClang = (kernel.stdenv.cc.isClang or false);
+  cc = if kernelUsesClang then llvmPackages.clang-unwrapped else gcc;
+in
+
+stdenvNoCC.mkDerivation {
   pname = "max98390-hda";
   version = "1.0-${kernel.version}";
 
   src = fetchFromGitHub {
     owner = "Andycodeman";
-    repo = "samsung-galaxy-book4-linux-fixes";
-    rev = "v0.3.12";
-    hash = "sha256-Mngm+2gBRxFv9d0W6t14qzpw2puvJjsqLPZntoYwBRo=";
+    repo = "samsung-galaxy-book-linux-fixes";
+    rev = "v0.3.26";
+    hash = "sha256-THezjEOxkaVnFY72zQyK2ER5VunOROm+i72JtSFCeMA=";
   };
 
   sourceRoot = "source/speaker-fix/src";
 
-  nativeBuildInputs = kernel.moduleBuildDependencies;
+  nativeBuildInputs =
+    kernel.moduleBuildDependencies
+    ++ [ cc ]
+    ++ lib.optionals kernelUsesClang [ llvmPackages.lld ];
 
   makeFlags = [
     "KVER=${kernel.modDirVersion}"
     "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+  ] ++ lib.optionals kernelUsesClang [
+    "LLVM=1"
+    "CC=${cc}/bin/clang"
+    "LD=${llvmPackages.lld}/bin/ld.lld"
   ];
 
   installPhase = ''
